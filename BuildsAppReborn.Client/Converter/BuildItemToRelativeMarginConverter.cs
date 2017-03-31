1408abd8d7fd479fea8cdc8aef6e4751db7642fd
ibvhefe <sebastian.mechelke@hotmail.de>
2017-03-31 10:03:56 +0200

    Made build history bar heights depending on their duration (#2)
    * Added tooltips to status bars
    
    * Added tooltips to status bars
    
    * The build history bars now each have a height that represents their build duration

---
+++ BuildsAppReborn.Client/Converter/BuildItemToRelativeMarginConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+using BuildsAppReborn.Contracts.Models;
+using BuildsAppReborn.Contracts.UI;
+
+namespace BuildsAppReborn.Client.Converter
+{
+    public class BuildItemToRelativeMarginConverter : IMultiValueConverter
+    {
+        #region Implementation of IMultiValueConverter
+
+        public Object Convert(Object[] values, Type targetType, Object parameter, CultureInfo culture)
+        {
+            var currentBuild = values[0] as IBuild;
+            var buildItems = values[1] as List<BuildItem>;
+            var height = (Double) values[2];
+            if (currentBuild != null && buildItems != null)
+            {
+                var durations = buildItems.Select(b => (b.Build.FinishDateTime - b.Build.StartDateTime).TotalSeconds);
+                var duration = (currentBuild.FinishDateTime - currentBuild.StartDateTime).TotalSeconds;
+                var barHeight = (height*duration)/durations.Max();
+                if (barHeight < 1)
+                {
+                    barHeight = 1;
+                }
+                var topMargin = height - barHeight;
+                return new Thickness(0, topMargin, 3, 0);
+            }
+            return default(Thickness);
+        }
+
+
+        public Object[] ConvertBack(Object value, Type[] targetTypes, Object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
