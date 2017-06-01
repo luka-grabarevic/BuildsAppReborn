72ae19b99833fac3139ac123c71ece9e721ca896
rb111 <rb111@users.noreply.github.com>
2017-06-01 16:42:55 +0200

    show artifacts context menu only when there are artifacts

---
+++ BuildsAppReborn.Client/Converter/CountToVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace BuildsAppReborn.Client.Converter
+{
+    public class CountToVisibilityConverter : IValueConverter
+    {
+        #region Implementation of IValueConverter
+
+        public Object Convert(Object value, Type targetType, Object parameter, CultureInfo culture)
+        {
+            var collection = value as ICollection;
+            if (collection != null && collection.Count > 0)
+                return Visibility.Visible;
+
+            return Visibility.Collapsed;
+        }
+
+        public Object ConvertBack(Object value, Type targetType, Object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        #endregion
+    }
+}
